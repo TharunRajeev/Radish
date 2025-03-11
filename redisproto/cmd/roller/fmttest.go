1ccd9a74058c9c5ac8a2418429cf96150dd48d5f
TharunRajeev <53303380+TharunRajeev@users.noreply.github.com>
2025-03-11 10:50:34 -0700

    feat: Making it compatible with redis protocols
    Making it compatible with redis protocols

---
+++ redisproto/cmd/roller/fmttest.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"log"
+
+	"github.com/tharunrajeev/radish/redisproto"
+)
+
+func main() {
+	bback := bytes.NewBuffer(nil)
+	buff := bufio.NewWriter(bback)
+	var c int64 = 1
+	redisproto.SendObjects(buff, []interface{}{[]byte("SUBSCRIBED"), []byte("rm"), c})
+	log.Println(bback.String())
+}
